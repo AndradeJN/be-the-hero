1b94d54a9422e743bc945ae8dcb1a1f1f67e90a2
AndradeJN <josenetogui@hotmail.com>
2020-04-24 15:20:43 -0300

    Validações de campos

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); // "./" para arquivo, "../" para voltar uma pasta
 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+
 /**
  * Rota / Recurso
  */
@@ -46,4 +50,4 @@ app.use(routes);
      */
 
 
-app.listen(3333);
+module.exports = app;
